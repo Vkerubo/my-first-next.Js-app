d7f4c6443563710e956ddbb29418bfab69bd5bfd
Valentine <kerubovalentine1@gmail.com>
2023-09-16 00:18:13 +0300

    rendering: static & dynamic

--- src/app/users/new/page.tsx
+++ src/app/users/new/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const New = () => {
-  return <div>New user!</div>;
+interface User {
+  id: string;
+  username: string;
+  email: string;
+}
+
+const New = async() => {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {next: {revalidate:10}});
+  const Users: User[] = await res.json();
+
+  return (
+    <>
+    <h1>New users</h1>
+    <ul>
+      {Users.map((user => <li key={user.id}>{user.username}: {user.email}</li>))}
+    </ul>
+    </>
+  );
 };
 
 export default New;
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 
-const UsersPage = () => {
-  return;
-  <div>page</div>;
+interface User {
+  id: string;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p> {/*Static data */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
